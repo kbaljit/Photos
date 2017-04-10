f9483e8ab7c874822d0a516ce225ba64ef1fc256
Milan Patel <Milan Patel>
2017-04-10 11:59:50 -0400

    Added Basic User Subsystem, modified login

--- Photos79/src/view/LoginController.java
+++ Photos79/src/view/LoginController.java
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 package view;
-
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
@@ -18,12 +16,11 @@ import javafx.stage.Stage;
 public class LoginController {
 	@FXML Button Login;
 	@FXML TextField username;
-	@FXML PasswordField Password;
+	@FXML PasswordField password;
 	
 	@FXML
 	private void buttonAction(ActionEvent E) throws IOException{
-		
-		if(username.getText().equals("admin")){
+		if(username.getText().equals("admin") && password.getText().equals("admin")){
 		Parent loginParent=FXMLLoader.load(getClass().getResource("/view/AdminSystem.fxml"));
 		Scene loginScene=new Scene(loginParent, 500, 500);
 		Stage loginStage=(Stage) ((Node) E.getSource()).getScene().getWindow();
@@ -33,54 +30,25 @@ public class LoginController {
 		loginStage.setScene(loginScene);
 		loginStage.show();
 		}
-		
-		
-	}
-	public void start(Stage mainStage){    
-		
-		
-
-	}
-
-}
-=======
-package view;
-
-import java.io.IOException;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-
-
-public class LoginController {
-	@FXML Button Login;
-	@FXML TextField User;
-	@FXML PasswordField Password;
-	
-	@FXML
-	private void buttonAction(ActionEvent E) throws IOException{
-		
-		if(User.getText().equals("admin")){
-			Parent loginParent=FXMLLoader.load(getClass().getResource("/view/AdminSystem.fxml"));
-			Scene loginScene=new Scene(loginParent, 500, 500);
+		else if((!username.getText().isEmpty()) && (!password.getText().isEmpty())){
+			
+			
+			Parent loginParent=FXMLLoader.load(getClass().getResource("/view/UserSystem.fxml"));
+			Scene loginScene=new Scene(loginParent, 700, 700);
 			Stage loginStage=(Stage) ((Node) E.getSource()).getScene().getWindow();
 			loginStage.hide();
-			loginStage.setTitle("Admin System");
-			loginStage.setResizable(false);
+		    loginStage.setTitle("User System");
+		    loginStage.setResizable(false);
 			loginStage.setScene(loginScene);
 			loginStage.show();
-		}else{
+		
+			
+		}
+		else{
 			
 		}
 		
+		
 	}
 	public void start(Stage mainStage){    
 		
@@ -89,4 +57,3 @@ public class LoginController {
 	}
 
 }
->>>>>>> f35a13e0d966547f018253c0773749c450ae197b
---
+++ Photos79/src/view/UserSystemController.java
@@ -0,0 +1,15 @@
+package view;
+
+import javafx.stage.Stage;
+
+public class UserSystemController {
+	
+	
+
+public void start(Stage mainStage){    
+		
+		
+
+	}
+
+}
